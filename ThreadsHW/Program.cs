6f0cbedb4a80a0e35542b1f67bea34e28a1962cb
yuurm <73988712+yuurm@users.noreply.github.com>
2023-06-26 14:08:19 +0300

    add strategy HW

--- ThreadsHW/Program.cs
+++ ThreadsHW/Program.cs
@@ -1,47 +1,69 @@
-﻿
-
-public class Product
+﻿public class Product
 {
     public string Name { get; set; }
     public decimal Price { get; set; }
+}
+
+public interface IDiscountStrategy
+{
+    decimal CalculateDiscount(decimal totalPrice);
+}
+
+public class QuantityDiscountStrategy : IDiscountStrategy
+{
+    public decimal CalculateDiscount(decimal totalPrice)
+    {
+        
+        return totalPrice * 0.1m; 
+    }
+}
+
+public class AmountDiscountStrategy : IDiscountStrategy
+{
+    public decimal CalculateDiscount(decimal totalPrice)
+    {
+        
+        return totalPrice * 0.05m; 
+    }
 }
 
 public class ProductCard
 {
     public Product Product { get; set; }
     public int Quantity { get; set; }
+    public IDiscountStrategy DiscountStrategy { get; set; }
+
+    public decimal CalculateDiscount()
+    {
+        decimal totalPrice = Product.Price * Quantity;
+        return DiscountStrategy.CalculateDiscount(totalPrice);
+    }
 }
 
 class Program
 {
     static void Main()
     {
-        
         ProductCard[] cards = {
             new() { Product = new Product { Name = "Apple", Price = 0.5m }, Quantity = 3 },
             new() { Product = new Product { Name = "Banana", Price = 0.3m }, Quantity = 2 },
             new() { Product = new Product { Name = "Orange", Price = 0.4m }, Quantity = 4 },
             new() { Product = new Product { Name = "Grapes", Price = 0.6m }, Quantity = 1 }
-            
         };
 
-        var sumOfAllProducts = cards.Sum(card => card.Product.Price * card.Quantity);
+        var quantityDiscountStrategy = new QuantityDiscountStrategy();
+        var amountDiscountStrategy = new AmountDiscountStrategy();
 
+        cards[0].DiscountStrategy = quantityDiscountStrategy;
+        cards[1].DiscountStrategy = quantityDiscountStrategy;
+        cards[2].DiscountStrategy = amountDiscountStrategy;
+        cards[3].DiscountStrategy = amountDiscountStrategy;
 
-        Console.WriteLine($"Sum of all products: {sumOfAllProducts}");
-
-        var sumsOfProductsPerBasket = new decimal[cards.Length];
-
-        
-        Parallel.For(0, cards.Length, i =>
+        foreach (var card in cards)
         {
-            sumsOfProductsPerBasket[i] = cards[i].Product.Price * cards[i].Quantity;
-        });
-
-        var sumOfProductsPerBasket = sumsOfProductsPerBasket.Sum();
-
-
-        Console.WriteLine($"Sum of products per basket: {sumOfProductsPerBasket}");
+            decimal discount = card.CalculateDiscount();
+            Console.WriteLine($"Discount for {card.Product.Name}: {discount}");
+        }
 
         Console.ReadLine();
     }
